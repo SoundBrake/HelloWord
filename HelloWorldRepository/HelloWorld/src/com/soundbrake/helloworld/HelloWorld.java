529453337b2315cd404fa2a489865265f53e793a
jeanx_000 <jeanx_000@MaryLaptop>
2016-10-28 17:57:33 -0400

    testing commit

---
+++ HelloWorldRepository/HelloWorld/src/com/soundbrake/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.soundbrake.helloworld;
+
+/**
+ * 
+ * @author:Mary Jiang
+ * @Date:
+ * @Email: mesuper222@gmail.com
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello, World!");
+	}
+
+	
+}
